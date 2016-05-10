e64cba21d950bd083b40cfdfb12ae187c6f1d02f
Timm Friebe <thekid@thekid.de>
2016-05-10 22:08:28 +0200

    Add completion for `use` statements

--- plugin.py
+++ plugin.py
@@ -1,5 +1,47 @@
 import os
 import sublime
+import sublime_plugin
+import subprocess
+import os
+
+class CompleteTypes(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        completions = []
+
+        line = view.substr(view.line(view.sel()[0]));
+        if line.startswith('use '):
+            dotted = line.replace('\\', '.')
+            package = dotted[4:-len(prefix) - 1]
+            search = dotted[4:-1]
+
+            # Find base path
+            base = os.path.dirname(view.file_name())
+            while not os.path.isfile(os.path.join(base, 'composer.json')):
+                parent = os.path.dirname(base)
+                if os.path.samefile(parent, base):
+                    break
+                base = parent
+
+            print('>> ' + dotted + '* @ ' + base);
+
+            # Start process
+            if os.name == 'nt':
+                info = subprocess.STARTUPINFO()
+                info.dwFlags |= subprocess.STARTF_USESHOWWINDOW 
+                info.wShowWindow = subprocess.SW_HIDE
+            else:
+                info = None
+
+            proc = subprocess.Popen('xp "' + os.path.join(os.path.dirname(__file__), 'types.script.php') + '" "' + package + '"', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, startupinfo=info, cwd=base)
+            output, error = proc.communicate()
+
+            # Transform output
+            for line in output.decode().split("\n"):
+                if (line.startswith(search)):
+                    completions.append(line.split('>>'));
+
+
+        return completions
 
 def disable_builtin_php():
     completions= os.path.join(sublime.packages_path(), 'PHP', 'PHP.sublime-completions')
---
+++ types.script.php
@@ -0,0 +1,33 @@
+<?php
+
+use lang\ClassLoader;
+
+function completionsIn($l, $path) {
+  $namespace= $path ? $path.'.' : '';
+  foreach ($l->packageContents($path) as $content) {
+    $end= strlen($content) - 1;
+    if ('/' === $content{$end}) {
+      $package= substr($content, 0, $end);
+      yield sprintf("1 %s%s (package)>>%s\\", $namespace, $package, strtr($package, '.', '\\'));
+    } else if (strstr($content, xp::CLASS_FILE_EXT)) {
+      $type= substr($content, 0, -strlen(xp::CLASS_FILE_EXT));
+      yield sprintf("2 %s%s (type)>>%s", $namespace, $type, strtr($type, '.', '\\'));
+    }
+  }
+}
+
+function completions($path) {
+  foreach (ClassLoader::getLoaders() as $l) {
+    if ($l->providesResource('autoload.php') || $l->providesResource('__xp.php')) {
+      foreach (completionsIn($l, $path) as $completion) {
+        yield $completion;
+      }
+    }
+  }
+}
+
+$completions= iterator_to_array(completions($argv[1]));
+sort($completions);
+foreach ($completions as $completion) {
+  echo substr($completion, 2), "\n";
+}
